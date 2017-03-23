226bd411a60b2b6b293e7f9d2414e8c6caa02d6c
jagadeeshpenta <jpenta@evoketechnologies.com>
2017-03-23 14:27:42 +0530

    login auth

--- src/app/Shared/auth.guard.ts
+++ src/app/Shared/auth.guard.ts
@@ -8,9 +8,16 @@ export class AuthGuard implements CanActivate {
     constructor(private _router: Router) { }
 
     canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        console.log("params: " + next.url[0].parameters);
+        console.log("path: " + next.url[0].path);
         if (next.url[0].path == 'login') {
             if (Cookie.get('auth_key')) {
                 console.log('You are already logged in');
+                // if(this._router.url == 'login'){ // if already logged in and typed /login in browser url
+                //      console.log("in");
+                //      this._router.navigate(['./dashboard']);
+                //        return false;
+                //  }
                 return false;
             }
             else {
--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../Shared/auth/auth.service';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html'
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent {
+loggedIn :boolean = false;
+  constructor(private auth: AuthService) { 
+    this.loggedIn = auth.isLoggedIn();
+  }
 
-  constructor() { }
-
-  ngOnInit() {
+  logout(){
+    this.auth.logout();
   }
 
 }
