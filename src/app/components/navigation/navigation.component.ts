0ee5e824225635a1cc016487bec620adc456fcb4
jagadeeshpenta <jpenta@evoketechnologies.com>
2017-03-24 01:00:03 +0530

    changes 1

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../Shared/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
-  templateUrl: './navigation.component.html'
+  templateUrl: './navigation.component.html' 
 })
 export class NavigationComponent {
+
 loggedIn :boolean = false;
-  constructor(private auth: AuthService) { 
-    this.loggedIn = auth.isLoggedIn();
+  constructor(private auth: AuthService, private _router: Router) { 
+   // this.loggedIn = auth.isLoggedIn();
+   console.log(auth.isLoggedIn());
   }
 
   logout(){
     this.auth.logout();
+    this._router.navigate(['login']);
   }
 
 }
