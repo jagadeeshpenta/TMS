57ff0162f99e92f310784c4adc677ef9566cfd45
Agilan Deenadayalan <adeenadayalan@evoketechnologies.com>
2017-03-27 19:05:53 +0530

    edit profile component update

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-export class User{
+export class User {
   id: number;
   name: string;
   username: string;
@@ -13,19 +13,45 @@ export class User{
 })
 export class ProfileComponent implements OnInit {
 
-  edited: boolean = true;
   users: User[] = [
-        {id: 25, name: 'Chris', username: 'sevilayha'}
-    ];
-  constructor() { }
+    { id: 25, name: 'Chris', username: 'sevilayha' }
+  ];
 
+
+  profile = {
+    firstname: 'muralikrishna',
+    lastname: 't',
+    dob: '1989-04-15'
+  };
+  isEdit = false;
+  editProfile;
   ngOnInit() {
   }
   activeUser: User;
-    selectUser(user) {
-        this.activeUser = user;
-        console.log(this.activeUser);
+  selectUser(user) {
+    this.activeUser = user;
+    console.log(this.activeUser);
+  }
+  eventHandler(event) {
+    if (event.keyCode === 13) {
+      console.log("Element")
+      this.activeUser.name = event.target.value;
+      console.log(this.activeUser);
     }
-    
+    if (event.keyCode == 27) {
+      console.log("Hello world")
+    }
+  }
 
+  doEditProfile():void {
+    this.isEdit = true;
+    this.editProfile = Object.create(this.profile);
+  }
+  handleSavechanges() {
+    console.log('chagnes ', this.editProfile, this.profile);
+    this.profile.firstname = this.editProfile.firstname;
+    this.profile.dob = this.editProfile.dob;
+    //TODO: Save changes to DB using API call
+    this.isEdit = false;
+  }
 }
